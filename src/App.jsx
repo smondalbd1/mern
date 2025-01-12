d4537355874902a19d94daacbe70b58559483295
Sumon Mondal <smondal.bd1@gmail.com>
2025-01-13 00:55:57 +0600

    First react App deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
-function item({name, isPacked=false}){ {
-    return 
-    <li className="item">{name}</li>;
-    
-}
-
-console.log(item.isPacked);
-
-
-export default function App(){
-  return(
-    <div className="app">
-      <h1>Items</h1>
-      <ul className="items">
-        <item isPacked={true} name="Sleeping Bag" />
-      </ul>
+export default function App() {
+  return (
+    <div>
+      <h1>My React App!</h1>
     </div>
-    
-  )
+  );
 }
